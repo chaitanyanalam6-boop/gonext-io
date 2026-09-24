a5dda52f842f1de0ff0c84cbcd7738805bed0994
Chaitanya Nalam <chaitanyanalam6@gmail.com>
2026-09-24 16:43:07 -0400

    Fix map tiles: switch from CARTO (now requires API key) to OpenStreetMap standard tiles

--- frontend/frontend/travel-planner-frontend/src/components/TripMap.tsx
+++ frontend/frontend/travel-planner-frontend/src/components/TripMap.tsx
@@ -57,10 +57,9 @@ export default function TripMap({ activities, selectedActivityId }: TripMapProps
   return (
     <MapContainer center={center} zoom={13} scrollWheelZoom className="trip-map">
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'
-        url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
-        subdomains="abcd"
-        maxZoom={20}
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        maxZoom={19}
       />
       <FlyToSelected activity={selectedActivity} />
       {withCoords.map((activity) => {
